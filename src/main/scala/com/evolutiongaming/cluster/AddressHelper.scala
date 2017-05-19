31bd9a058406b35579759b9f6d44e00001c1d4ab
Yaroslav Klymko <t3hnar@gmail.com>
2017-05-19 12:02:12 +0300

    simplify AddressHelper

--- src/main/scala/com/evolutiongaming/cluster/AddressHelper.scala
+++ src/main/scala/com/evolutiongaming/cluster/AddressHelper.scala
@@ -1,32 +1,14 @@
 package com.evolutiongaming.cluster
 
 import akka.actor.{Address, ExtendedActorSystem, Extension, ExtensionId}
-import com.evolutiongaming.util.ConfigHelper._
-import com.typesafe.config.Config
 
 class AddressHelper(system: ExtendedActorSystem) extends Extension {
 
-  lazy val tcpAddress: Address = {
-    val (host, port) = {
-      val conf = system.settings.config.get[Config]("akka.remote.netty.tcp")
-      val result = for {
-        host <- conf.get[Option[String]]("hostname")
-        port <- conf.get[Option[Int]]("port")
-      } yield (host, port)
-
-      result getOrElse ("127.0.0.1" -> 2552)
-    }
-
-    Address(
-      protocol = "akka.tcp",
-      system = system.name,
-      host = host,
-      port = port)
-  }
+  lazy val address: Address = system.provider.getDefaultAddress
 
   implicit class RichAddress(val self: Address) {
-    def local: Address = if (self == tcpAddress) self.copy(host = None, port = None, protocol = "akka") else self
-    def global: Address = if (self.hasGlobalScope) self else tcpAddress
+    def local: Address = if (self == address) self.copy(host = None, port = None, protocol = "akka") else self
+    def global: Address = if (self.hasGlobalScope) self else address
   }
 }
 
