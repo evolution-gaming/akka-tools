e0f6a14bf1d63b45848fe4a98a4dadc18e6d1135
Yaroslav Klymko <t3hnar@gmail.com>
2017-11-30 22:41:37 +0100

    PL-6222 Rework MappedAllocationStrategy in order to fix infinite rebalance

--- cluster/src/main/scala/com/evolutiongaming/cluster/MappedAllocationStrategy.scala
+++ cluster/src/main/scala/com/evolutiongaming/cluster/MappedAllocationStrategy.scala
@@ -1,74 +1,70 @@
 package com.evolutiongaming.cluster
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef, ActorSystem, Address, ExtendedActorSystem, Extension, ExtensionId}
+import akka.cluster.ddata.LWWMapKey
 import akka.cluster.sharding.ShardCoordinator.ShardAllocationStrategy
 import akka.cluster.sharding.ShardRegion
+import com.evolutiongaming.concurrent.CurrentThreadExecutionContext
 import com.typesafe.scalalogging.LazyLogging
 
-import scala.collection.immutable
+import scala.collection.concurrent.TrieMap
+import scala.collection.immutable.IndexedSeq
 import scala.concurrent.Future
 
-class MappedAllocationStrategy(
+class MappedAllocationStrategy private(
   typeName: String,
-  fallbackStrategy: ShardAllocationStrategy,
-  proxy: ActorRef,
-  val maxSimultaneousRebalance: Int)(implicit system: ActorSystem)
-  extends ShardAllocationStrategy with LazyLogging {
+  val maxSimultaneousRebalance: Int,
+  mapping: MappedAllocationStrategy.Mapping,
+  toAddress: ActorRef => Address,
+  fallbackStrategy: ShardAllocationStrategy) extends ShardAllocationStrategy with LazyLogging {
 
-  import MappedAllocationStrategy._
-
-  private val addressHelper = AddressHelperExtension(system)
-
-  def mapShardToRegion(shardId: ShardRegion.ShardId, regionRef: ActorRef) =
-    proxy ! UpdateMapping(typeName, shardId, regionRef)
+  def mapShardToAddress(shardId: ShardRegion.ShardId, address: Address): Unit = {
+    mapping.set(shardId, address)
+  }
 
   def allocateShard(
     requester: ActorRef,
     shardId: ShardRegion.ShardId,
-    currentShardAllocations: Map[ActorRef, immutable.IndexedSeq[ShardRegion.ShardId]]): Future[ActorRef] = {
+    current: Map[ActorRef, IndexedSeq[ShardRegion.ShardId]]): Future[ActorRef] = {
 
-    val result = (shardToRegionMapping get EntityKey(typeName, shardId)) flatMap { toNode =>
-      val currentRegions = currentShardAllocations.keySet
-      if (currentRegions contains toNode)
-        Some(toNode)
-      else {
-        val toNodeHost = (addressHelper toGlobal toNode.path.address).host
-        currentRegions find (ref => (addressHelper toGlobal ref.path.address).host == toNodeHost)
-      }
-    }
+    val region = for {
+      address <- mapping get shardId
+      region <- current collectFirst { case (region, _) if toAddress(region) == address => region }
+    } yield region
 
-    result match {
-      case Some(toNode) =>
-        logger debug s"AllocateShard $typeName\n\t" +
+    region match {
+      case Some(region) =>
+        logger debug s"allocateShard $typeName\n\t" +
           s"shardId:\t$shardId\n\t" +
-          s"on node:\t$toNode\n\t" +
+          s"region:\t$region\n\t" +
           s"requester:\t$requester\n\t"
-        Future successful toNode
-      case None         =>
-        logger debug s"AllocateShard fallback $typeName, shardId:\t$shardId"
-        fallbackStrategy.allocateShard(requester, shardId, currentShardAllocations)
+        Future.successful(region)
+
+      case None =>
+        logger debug s"allocateShard fallback $typeName, shardId:\t$shardId"
+        fallbackStrategy.allocateShard(requester, shardId, current).map { region =>
+          mapping.set(shardId, toAddress(region))
+          region
+        }(CurrentThreadExecutionContext)
     }
   }
 
   def rebalance(
-    currentShardAllocations: Map[ActorRef, immutable.IndexedSeq[ShardRegion.ShardId]],
-    rebalanceInProgress: Set[ShardRegion.ShardId]): Future[Set[ShardRegion.ShardId]] = {
+    current: Map[ActorRef, IndexedSeq[ShardRegion.ShardId]],
+    inProgress: Set[ShardRegion.ShardId]): Future[Set[ShardRegion.ShardId]] = {
 
-    logger debug
-      s"rebalance $typeName: currentShardAllocations = $currentShardAllocations, rebalanceInProgress = $rebalanceInProgress"
-
-    val shardsToRebalance = for {
-      (ref, shards) <- currentShardAllocations
+    val toRebalance = for {
+      (ref, shards) <- current
       shardId <- shards
-      targetRef <- shardToRegionMapping get EntityKey(typeName, shardId)
-      if targetRef != ref
+      address <- mapping get shardId
+      if toAddress(ref) != address
     } yield shardId
 
-    val result = (shardsToRebalance.toSet -- rebalanceInProgress) take maxSimultaneousRebalance
+    val result = (toRebalance.toSet -- inProgress) take maxSimultaneousRebalance
 
-    if (result.nonEmpty) logger info s"Rebalance $typeName\n\t" +
-      s"current:${ currentShardAllocations.mkString("\n\t\t", "\n\t\t", "") }\n\t" +
-      s"rebalanceInProgress:\t$rebalanceInProgress\n\t" +
+    if (result.nonEmpty) logger info s"rebalance $typeName\n\t" +
+      s"current:${ current.mkString("\n\t\t", "\n\t\t", "") }\n\t" +
+      s"inProgress:\t$inProgress\n\t" +
       s"result:\t$result"
 
     Future successful result
@@ -82,31 +78,52 @@ object MappedAllocationStrategy {
     fallbackStrategy: ShardAllocationStrategy,
     maxSimultaneousRebalance: Int)
     (implicit system: ActorSystem): MappedAllocationStrategy = {
-    // proxy doesn't depend on typeName, it should just start once
-    val proxy = MappedAllocationStrategyDDProxy(system).ref
+
+    val mapping = MappingExtension(system).get(typeName)
+
+    val addressHelper = AddressHelperExtension(system)
+
     new MappedAllocationStrategy(
-      typeName = typeName,
-      fallbackStrategy = fallbackStrategy,
-      proxy = proxy,
-      maxSimultaneousRebalance = maxSimultaneousRebalance)
+      typeName,
+      maxSimultaneousRebalance,
+      mapping,
+      (ref: ActorRef) => addressHelper toGlobal ref.path.address,
+      fallbackStrategy)
   }
 
-  case class EntityKey(typeName: String, id: ShardRegion.ShardId) {
-    override def toString: String = s"$typeName#$id"
+
+  trait Mapping {
+    def get(shardId: ShardRegion.ShardId): Option[Address]
+    def set(shardId: ShardRegion.ShardId, address: Address): Unit
   }
 
-  object EntityKey {
-    def unapply(arg: List[String]): Option[EntityKey] = arg match {
-      case typeName :: id :: Nil => Some(EntityKey(typeName, id))
-      case _                     => None
+  object Mapping {
+    def apply(typeName: String, system: ActorSystem): Mapping = {
+
+      val dataKey = LWWMapKey[ShardRegion.ShardId, Address](s"ShardToRegion-$typeName")
+      val cache = ReplicatedCache(dataKey, system)
+
+      new Mapping {
+        def set(shardId: ShardRegion.ShardId, address: Address): Unit = {
+          cache.put(shardId, address)
+        }
+        def get(shardId: ShardRegion.ShardId): Option[Address] = {
+          cache.get(shardId)
+        }
+      }
     }
-
-    def unapply(arg: String): Option[EntityKey] = unapply((arg split "#").toList)
   }
 
-  case class UpdateMapping(typeName: String, id: ShardRegion.ShardId, regionRef: ActorRef)
-  case class Clear(typeName: String, id: ShardRegion.ShardId)
 
-  @volatile
-  private[cluster] var shardToRegionMapping: Map[EntityKey, ActorRef] = Map.empty
+  class MappingExtension(system: ActorSystem) extends Extension {
+    private val cache = TrieMap.empty[String, Mapping]
+
+    def get(typeName: String): Mapping = {
+      cache.getOrElseUpdate(typeName, Mapping(typeName, system))
+    }
+  }
+
+  object MappingExtension extends ExtensionId[MappingExtension] {
+    def createExtension(system: ExtendedActorSystem): MappingExtension = new MappingExtension(system)
+  }
 }
