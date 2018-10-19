c49343d502a261802171832b38bfb2beb716d284
snovgorodtcev <snovgorodtcev@evolutiongaming.com>
2018-10-19 16:41:13 +0200

    consider role in NodesUnreachableException instantiation

--- cluster/src/main/scala/com/evolutiongaming/cluster/NodesUnreachableException.scala
+++ cluster/src/main/scala/com/evolutiongaming/cluster/NodesUnreachableException.scala
@@ -9,23 +9,19 @@ class NodesUnreachableException(addresses: Nel[Address], cause: Throwable) exten
 
   override def getCause = cause
 
-  override def getMessage = {
-    addresses map { _.hostPort } match {
-      case Nel(node, Nil) => s"node $node is unreachable"
-      case nodes          => s"nodes ${ nodes mkString ", " } are unreachable"
-    }
+  override def getMessage = addresses match {
+    case Nel(node, Nil) => s"node $node is unreachable"
+    case nodes          => s"nodes ${ nodes mkString ", " } are unreachable"
   }
 }
 
 object NodesUnreachableException {
 
-  def opt(timeoutException: TimeoutException, system: ActorSystem): Option[NodesUnreachableException] = {
-    for {
-      cluster <- ClusterOpt(system)
-      members <- Nel.opt(cluster.state.unreachable)
-    } yield {
-      val addresses = for {member <- members} yield member.address
-      new NodesUnreachableException(addresses, timeoutException)
-    }
-  }
+  def opt(timeoutException: TimeoutException, system: ActorSystem, role: String): Option[NodesUnreachableException] =
+    ClusterOpt(system).flatMap(c =>
+      Nel
+        .opt(c.state.unreachable.collect { case m if m.roles.contains(role) => m.address })
+        .map(new NodesUnreachableException(_, timeoutException))
+    )
+
 }
