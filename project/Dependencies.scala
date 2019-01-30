eebc90e16bba7c5da711709f93c0452f0dc65021
Yaroslav Klymko <t3hnar@gmail.com>
2019-01-30 09:32:32 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Akka {
-    private val version = "2.5.18"
+    private val version = "2.5.20"
     val Stream          = "com.typesafe.akka" %% "akka-stream" % version
     val Actor           = "com.typesafe.akka" %% "akka-actor" % version
     val AkkaPersistence = "com.typesafe.akka" %% "akka-persistence" % version
@@ -18,7 +18,7 @@ object Dependencies {
   val Logging       = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
   val ChillAkka     = "com.twitter" %% "chill-akka" % "0.9.3" excludeAll ExclusionRule("org.ow2.asm", "asm")
   val scalax        = "com.github.t3hnar" %% "scalax" % "3.4"
-  val playJsonTools = "com.evolutiongaming" %% "play-json-tools" % "0.3.8"
+  val playJsonTools = "com.evolutiongaming" %% "play-json-tools" % "0.3.9"
   val Nel           = "com.evolutiongaming" %% "nel" % "1.3.3"
   val MetricTools   = "com.evolutiongaming" %% "metric-tools" % "1.1"
   val ScalaTools    = "com.evolutiongaming" %% "scala-tools" % "2.0"
