4d52ca0f5e512fa7827d37d45f283c6fd1c38940
Yaroslav Klymko <t3hnar@gmail.com>
2019-05-09 23:11:26 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,23 +3,24 @@ import sbt._
 object Dependencies {
 
   object Akka {
-    private val version = "2.5.21"
-    val Stream          = "com.typesafe.akka" %% "akka-stream" % version
-    val Actor           = "com.typesafe.akka" %% "akka-actor" % version
-    val AkkaPersistence = "com.typesafe.akka" %% "akka-persistence" % version
-    val Cluster         = "com.typesafe.akka" %% "akka-cluster" % version % Compile
-    val ClusterSharding = "com.typesafe.akka" %% "akka-cluster-sharding" % version % Compile
-    val TestKit         = "com.typesafe.akka" %% "akka-testkit" % version
+    private val version = "2.5.22"
+    val Stream          = "com.typesafe.akka" %% "akka-stream"           % version
+    val Actor           = "com.typesafe.akka" %% "akka-actor"            % version
+    val AkkaPersistence = "com.typesafe.akka" %% "akka-persistence"      % version
+    val Cluster         = "com.typesafe.akka" %% "akka-cluster"          % version
+    val ClusterSharding = "com.typesafe.akka" %% "akka-cluster-sharding" % version
+    val TestKit         = "com.typesafe.akka" %% "akka-testkit"          % version
   }
 
-  val ScalaTest     = "org.scalatest" %% "scalatest" % "3.0.7"
-  val MetricsCore   = "io.dropwizard.metrics" % "metrics-core" % "3.2.6"
-  val Logback       = "ch.qos.logback" % "logback-classic" % "1.2.3"
-  val Logging       = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
-  val scalax        = "com.github.t3hnar" %% "scalax" % "3.4"
+  val ScalaTest   = "org.scalatest"              %% "scalatest"       % "3.0.7"
+  val MetricsCore = "io.dropwizard.metrics"       % "metrics-core"    % "3.2.6"
+  val Logback     = "ch.qos.logback"              % "logback-classic" % "1.2.3"
+  val scalax      = "com.github.t3hnar"          %% "scalax"          % "3.4"
+  val Logging     = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.2"
+
   val playJsonTools = "com.evolutiongaming" %% "play-json-tools" % "0.3.9"
-  val Nel           = "com.evolutiongaming" %% "nel" % "1.3.3"
-  val MetricTools   = "com.evolutiongaming" %% "metric-tools" % "1.1"
-  val ScalaTools    = "com.evolutiongaming" %% "scala-tools" % "2.0"
-  val ConfigTools   = "com.evolutiongaming" %% "config-tools" % "1.0.3"
+  val Nel           = "com.evolutiongaming" %% "nel"             % "1.3.3"
+  val MetricTools   = "com.evolutiongaming" %% "metric-tools"    % "1.1"
+  val ScalaTools    = "com.evolutiongaming" %% "scala-tools"     % "2.0"
+  val ConfigTools   = "com.evolutiongaming" %% "config-tools"    % "1.0.3"
 }
