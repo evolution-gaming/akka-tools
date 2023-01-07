99b8c1d7600df4c4686d08655284469c1e7ebafb
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 17:44:49 +0000

    Applied Scalafix rule(s) https://raw.githubusercontent.com/scalatest/autofix/e4de53fa40fac423bd64d165ff36bde38ce52388/3.1.x/rules/src/main/scala/org/scalatest/autofix/v3_1_x/RewriteDeprecatedNames.scala
    See https://github.com/scalatest/autofix/blob/master/3.1.x/README.md for details

--- serialization/src/test/scala/com/evolutiongaming/serialization/SerializerOfSpec.scala
+++ serialization/src/test/scala/com/evolutiongaming/serialization/SerializerOfSpec.scala
@@ -2,9 +2,10 @@ package com.evolutiongaming.serialization
 
 import akka.serialization.ByteArraySerializer
 import com.evolutiongaming.test.ActorSpec
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class SerializerOfSpec extends WordSpec with Matchers with ActorSpec {
+class SerializerOfSpec extends AnyWordSpec with Matchers with ActorSpec {
   "SerializerOf" should {
     "find serializer of type" in new ActorScope {
       val serializer = SerializerOf[ByteArraySerializer](system)
