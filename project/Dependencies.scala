1f087250d34e4fadf6c42285e2067c81f9d90d17
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-08-11 09:26:05 +0200

    Update scala-logging to 3.9.6 (#151)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   val ScalaTest     = "org.scalatest"              %% "scalatest"       % "3.2.20"
   val Logback       = "ch.qos.logback"              % "logback-classic" % "1.2.3"
-  val Logging       = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"
+  val Logging       = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.6"
   val Nel           = "com.evolutiongaming"        %% "nel"             % "1.3.5"
   val ScalaTools    = "com.evolutiongaming"        %% "scala-tools"     % "3.0.6"
   val ConfigTools   = "com.evolutiongaming"        %% "config-tools"    % "1.0.5"
